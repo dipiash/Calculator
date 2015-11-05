a1fee150f5f07824d04d05520ac5d78d78ea8e86
Dm <DmitriyTemn@gmail.com>
2015-11-05 13:41:57 +0500

    Added empty methods for Calculatro library

---
+++ Calculator/RevPolNotation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class RevPolNotation
+    {
+        private static string _inputExpression = string.Empty;
+        private static string _outputExpression = string.Empty;
+
+        public static string GetRPNExpression(string inputExpression)
+        {
+            // evaluate some methods
+
+            return _outputExpression;
+        }
+
+        private static bool IsSimpleOperator(char inputOperator)
+        {
+            // some code for check
+
+            return false;
+        }
+
+        private static byte GetPriorytyForItem(string inputItem)
+        {
+            // some code for check
+
+            return 0;
+        }
+
+        private static string ConvertExpressionToRPN(string inputExpression)
+        {
+            // some code for converting
+
+            return "";
+        }
+    }
+}
