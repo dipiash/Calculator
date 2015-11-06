baeb2bc7ac1aa77a43f6550e39c50ea4cef51ee4
Dm <DmitriyTemn@gmail.com>
2015-11-06 20:40:17 +0500

    Created new class for prepare and check input Expression

---
+++ Calculator/PrepInputExpression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class PrepInputExpression
+    {
+        private static string _outputexpression = string.Empty;
+        public static string PrepareExpression(string inputExpression)
+        {
+            // some code for prepare
+
+            return _outputexpression;
+        }
+
+        // Add zero to end|start string, if we have simple operator at start|end
+
+        // Check quotes
+
+        // check digits|operators|quotes
+
+        // Trim
+
+        // Check sequence
+            // ++|+-|+*|+/
+            // --|-+|-*|-/
+            // **|*+|*-|*/
+            // //|/-|/+|/*
+            // ()
+    }
+}
