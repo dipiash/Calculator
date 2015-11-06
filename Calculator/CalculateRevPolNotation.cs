7ad23dc4cfe1255d9b072a0f7f9f1dfdead7ef21
Dm <DmitriyTemn@gmail.com>
2015-11-06 20:56:07 +0500

    Added new method for calculate RPN Expression

---
+++ Calculator/CalculateRevPolNotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculateRevPolNotation
+    {
+        public static decimal _result = decimal.Zero;
+        public static decimal GetResult(string inputRPNExpression)
+        {
+            return _result;
+        }
+
+        // Method for add
+
+        // Method for sub
+
+        // Method for div
+
+        // Method for mul
+    }
+}
