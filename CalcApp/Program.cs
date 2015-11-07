270a06c94708b3552089a9b948d77fe15d35d1da
Dm <DmitriyTemn@gmail.com>
2015-11-08 00:46:22 +0500

    Fixed error. Added check to empty expression.

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -13,26 +13,34 @@ namespace CalcApp
                 Console.WriteLine("Input expression. Use only [ /*-+.() ].");
                 string expression = Console.ReadLine();
 
-                PrepInputExpression prepare = new PrepInputExpression();
-                string tmpExpression = prepare.PrepareExpression(expression);
-                List<string> errors = prepare.ValidateInputExpression(tmpExpression);
-                int countErrors = errors.Count;
-
-                if (countErrors != 0)
+                if (expression.Length == 0)
                 {
-                    Console.WriteLine("\nFounded {0} error.", countErrors);
-                    int item;
-                    for (item = 0; item < countErrors; item++)
+                    Console.WriteLine("Error! You should input expression!");
+                }
+                else
+                {
+                    PrepInputExpression prepare = new PrepInputExpression();
+                    string tmpExpression = prepare.PrepareExpression(expression);
+                    List<string> errors = prepare.ValidateInputExpression(tmpExpression);
+                    int countErrors = errors.Count;
+
+                    if (countErrors != 0)
                     {
-                        Console.WriteLine(errors[item]);
+                        Console.WriteLine("\nFounded {0} error.", countErrors);
+                        int item;
+                        for (item = 0; item < countErrors; item++)
+                        {
+                            Console.WriteLine(errors[item]);
+                        }
                     }
-                } else
-                {
-                    tmpExpression = RevPolNotation.GetRPNExpression(tmpExpression);
-                    decimal result = CalculateRevPolNotation.Calculate(tmpExpression);
+                    else
+                    {
+                        tmpExpression = RevPolNotation.GetRPNExpression(tmpExpression);
+                        decimal result = CalculateRevPolNotation.Calculate(tmpExpression);
 
-                    Console.WriteLine("Evaluate expression: {0}", expression);
-                    Console.WriteLine("result: {0}", result);
+                        Console.WriteLine("Evaluate expression: {0}", expression);
+                        Console.WriteLine("result: {0}", result);
+                    }
                 }
             }
         }
