52b4ec79889bcd918ba0a801167b90f5c083b896
Dm <DmitriyTemn@gmail.com>
2015-11-07 19:58:02 +0500

    Writed a new tests for checkQuotes

--- Calculator.Tests/PrepInputExpression.Tests.cs
+++ Calculator.Tests/PrepInputExpression.Tests.cs
@@ -42,5 +42,26 @@ namespace Calculator.Tests
             // count errors in _logError should be 4, because fiveExpression is correct
             Assert.AreEqual(testPrep.ValidateInputExpression(fiveExpression).Count, 4);
         }
+
+        [TestMethod]
+        public void Test_ValidateInputexpression_CheckQuotes()
+        {
+            // incorrect
+            string oneExpression = "((2+3)*2+1/10";
+            string twoExpression = "(1+1)/3)+1)/4)";
+
+            // correct
+            string threeExpression = "((1+(1)))";
+            string fourExpression = "1+(2+3)/5+(1+1)/2";
+
+            PrepInputExpression testPrep = new PrepInputExpression();
+
+            Assert.AreEqual(testPrep.ValidateInputExpression(oneExpression).Count, 1);
+            Assert.AreEqual(testPrep.ValidateInputExpression(twoExpression).Count, 2);
+
+            // Count errors in _logError should be 2, because three* and four* is correct
+            Assert.AreEqual(testPrep.ValidateInputExpression(threeExpression).Count, 2);
+            Assert.AreEqual(testPrep.ValidateInputExpression(fourExpression).Count, 2);
+        }
     }
 }
