dd1097f849032eacb7869c3eb1f856a4e845a0e1
Dm <DmitriyTemn@gmail.com>
2015-11-07 13:01:46 +0500

    Writed test for prepare expression class

--- Calculator.Tests/PrepInputExpression.Tests.cs
+++ Calculator.Tests/PrepInputExpression.Tests.cs
@@ -7,8 +7,15 @@ namespace Calculator.Tests
     public class PrepInputExpressionTests
     {
         [TestMethod]
-        public void Test()
+        public void Test_RepairStartExpression()
         {
+            string oneExpression = "+2";
+            string twoExpression = "2+2";
+            string threeExpression = "-2";
+
+            Assert.AreEqual(PrepInputExpression.PrepareExpression(oneExpression), "0+2");
+            Assert.AreEqual(PrepInputExpression.PrepareExpression(twoExpression), "2+2");
+            Assert.AreEqual(PrepInputExpression.PrepareExpression(threeExpression), "0-2");
         }
     }
 }
