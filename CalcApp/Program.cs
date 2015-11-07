a3c2e603d07dbaadecc15eb334f395cdd448d03c
Dm <DmitriyTemn@gmail.com>
2015-11-08 00:40:03 +0500

    Writed code for calculate expression in console
    But now we don't catch error if expression contain sequence of simple
    operators. 1++++2 or 1****2. Don't show errors.

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -11,6 +11,33 @@ namespace CalcApp
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Input expression. Use only [ /*-+.() ].");
+                string expression = Console.ReadLine();
+
+                PrepInputExpression prepare = new PrepInputExpression();
+                string tmpExpression = prepare.PrepareExpression(expression);
+                List<string> errors = prepare.ValidateInputExpression(tmpExpression);
+                int countErrors = errors.Count;
+
+                if (countErrors != 0)
+                {
+                    Console.WriteLine("\nFounded {0} error.", countErrors);
+                    int item;
+                    for (item = 0; item < countErrors; item++)
+                    {
+                        Console.WriteLine(errors[item]);
+                    }
+                } else
+                {
+                    tmpExpression = RevPolNotation.GetRPNExpression(tmpExpression);
+                    decimal result = CalculateRevPolNotation.Calculate(tmpExpression);
+
+                    Console.WriteLine("Evaluate expression: {0}", expression);
+                    Console.WriteLine("result: {0}", result);
+                }
+            }
         }
     }
 }
