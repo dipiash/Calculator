3a711535ca24c07aef721be246e0cffcfe91d7e9
Dm <DmitriyTemn@gmail.com>
2015-11-08 16:13:46 +0500

    Added some comments

--- Calculator/ExpresionExceptions.cs
+++ Calculator/ExpresionExceptions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace Calculator
 {
+    /// <summary>
+    /// Simple custom class for handling exceptions for our application.
+    /// </summary>
     public class ExpresionExceptions : ApplicationException
     {
         public ExpresionExceptions() { }
